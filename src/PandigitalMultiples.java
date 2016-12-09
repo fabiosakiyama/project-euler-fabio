7d60608f15826943a74b67ad53424a34e7c6e035
Fábio Sakiyama <ZSAKFAB@ericsson.com>
2016-12-09 11:25:35 -0200

    pandiital multiples

--- src/PandigitalMultiples.java
+++ src/PandigitalMultiples.java
@@ -1,35 +1,43 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class PandigitalMultiples {
-
+	
 	public static void main(String[] args) {
-
-		int[] a = new int[]{1,2,3};
-
-		printPermutations(3, a);
 		
-	}
-
-	public static void printPermutations(int n, int[] a) {
-		if (n == 1) {
-			for (int i : a) {
-				System.out.print(i);
-			}
-			System.out.println();
-		} else {
-			for (int i = 0; i < n - 1; i++) {
-				printPermutations(n - 1, a);
-				if (n % 2 == 0) {
-					int ai = a[i];
-					a[i] = a[n-1];
-					a[n-1] = ai;
-				} else {
-					int ai = a[0];
-					a[0] = a[n-1];
-					a[n-1] = ai;
+		long maiorNumero = 0;
+		Set<String> numerosJaUsados = new HashSet<>();
+		String valorTotal = "";
+		for(int i = 1; i < 9999999; i++){
+			for(int j = 1; j< 9999999; j++){
+				int result = i * j;
+				String produto = Integer.toString(result);
+				
+				if((valorTotal.length() + produto.length()) > 9){
+					// Ja passou dos 9 dígitos
+					break;
+				}else{
+					valorTotal = valorTotal  + (i * j);
+				}
+				
+				for(String c : produto.split("")){
+					if(c.equals("0") || numerosJaUsados.contains(c)){
+						// Ja nao é mais pandigital
+						break;
+					}
+					numerosJaUsados.add(c);
 				}
 			}
-			printPermutations(n - 1, a);
+			if(valorTotal.length() == 9 && numerosJaUsados.size() == 9){
+				long parseLong = Long.parseLong(valorTotal);
+				if(maiorNumero < parseLong){
+					maiorNumero = parseLong;
+				}
+			}
+			valorTotal = "";
+			numerosJaUsados.clear();
 		}
+		System.out.println(maiorNumero);
+		System.out.println("fim");
 	}
-
 }
