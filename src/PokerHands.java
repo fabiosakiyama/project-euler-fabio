cee4def7b28ae430ba761fd777bc31d01ac2832f
Fábio Sakiyama <ZSAKFAB@ericsson.com>
2016-12-13 16:04:59 -0200

    Poker

--- src/PokerHands.java
+++ src/PokerHands.java
@@ -3,11 +3,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Comparator;
 import java.util.List;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class PokerHands {
 
@@ -23,6 +20,8 @@ public class PokerHands {
 			}
 			Dealer.showWinner();
 		}
+		
+		System.out.println("PLAYER 1 WINS: " + Dealer.player1Wins);
 	}
 
 	private static List<String> readCardsInLine() {
@@ -70,6 +69,15 @@ class Dealer {
 		// calcula pontuacao
 		int player1Score = getHandScore(player1cards);
 		int player2Score = getHandScore(player2cards);
+		if(player1Score == player2Score){
+			player1Score += getHighestCard(player1cards);
+			player2Score += getHighestCard(player2cards);
+			while(player1Score == player2Score){
+				player1cards.remove(getHighestCard(player1cards));
+				player2cards.remove(getHighestCard(player2cards));
+			}
+		}
+		
 		if (player1Score > player2Score) {
 			System.out.println("Player 1 Wins");
 			player1Wins++;
@@ -86,33 +94,75 @@ class Dealer {
 		}
 
 		if (isStraightFlush(cards)) {
-			return 950;
+			return 900 + getHighestCard(cards);
 		}
 
 		if (isFourOfAKind(cards)) {
-			return 900;
+			return 800 + getPairValue(cards, 4);
 		}
 
 		if (isFullHouse(cards)) {
-			return 850;
+			return 600 + (getPairValue(cards, 3) * 8) + getPairValue(cards, 2);
 		}
 		
 		if(isFlush(cards)){
-			return 800;
+			return 500 + getHighestCard(cards);
 		}
 
 		if(isStraight(cards)){
-			return 750;
+			return 400 + getHighestCard(cards);
 		}
 		
 		if(isThreeOfAKind(cards)){
-			return 700;
+			return 300 + getPairValue(cards, 3);
 		}
-		return score;
+		
+		if(isTwoPairs(cards)){
+			return 200 + getPairValue(cards, 2);
+		}
+		
+		if(isOnePair(cards)){
+			return 100 + getPairValue(cards, 2);
+		}
+		return score + getHighestCard(cards);
+	}
+
+	private static Integer getPairValue(List<Card> cards, int x) {
+		Collection<List<Card>> values = cards.stream().collect(Collectors.groupingBy(Card::getValue)).values();
+		List<List<Card>> collect = values.stream().filter(c -> c.size() == x).collect(Collectors.toList());
+		if(collect.size() == 2){
+			Integer cardValue1 = Integer.parseInt(getValue(collect.get(0).get(0).getValue()));
+			Integer cardValue2 = Integer.parseInt(getValue(collect.get(1).get(0).getValue()));
+			if(cardValue1 > cardValue2){
+				return cardValue1;
+			}
+			else{
+				return cardValue2;
+			}
+		}
+		String cardValue = collect.get(0).get(0).getValue();
+		int value = Integer.parseInt(getValue(cardValue));
+		return value;
+	}
+
+	private static boolean isOnePair(List<Card> cards) {
+		if (hasXRepeatedCards(cards, 2) && !hasXRepeatedCards(cards, 3)) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean isTwoPairs(List<Card> cards) {
+		if (hasXRepeatedCardsTwice(cards)) {
+			return true;
+		}
+		return false;
 	}
 
 	private static boolean isThreeOfAKind(List<Card> cards) {
-		// TODO Auto-generated method stub
+		if (hasXRepeatedCards(cards, 3) && !hasXRepeatedCards(cards, 2)) {
+			return true;
+		}
 		return false;
 	}
 
@@ -162,42 +212,63 @@ class Dealer {
 
 		return true;
 	}
+	
+	private static Integer getHighestCard(List<Card> cards) {
+		List<Integer> values = new ArrayList<>();
+		cards.stream().forEach(c -> values.add(Integer.parseInt(getValue(c.getValue()))));
+		List<Integer> orderedValues = values.stream().sorted().collect(Collectors.toList());
+		return orderedValues.get(orderedValues.size()- 1);
+	}
 
 	private static boolean hasSequence(List<Card> cards) {
 		List<Integer> values = new ArrayList<>();
-		cards.stream().forEach(c -> values.add(Integer.parseInt(getValue(c))));
+		boolean hasAce = false;
+		int sequenceCounter = 0;
+		cards.stream().forEach(c -> values.add(Integer.parseInt(getValue(c.getValue()))));
 		List<Integer> orderedValues = values.stream().sorted().collect(Collectors.toList());
 		for (int i = 0; i < orderedValues.size() - 1; i++) {
+			// ESPECIAL CASE ACE
+			if(orderedValues.get(orderedValues.size() - 1) == 14){
+				hasAce = true;
+			}
+			
 			if (!(orderedValues.get(i) + 1 == orderedValues.get(i + 1))) {
+				if(sequenceCounter == 3 && hasAce){
+					if(orderedValues.get(0) == 2){
+						return true;
+					}
+				}
 				return false;
+			}else{
+				sequenceCounter++;
 			}
 		}
 
 		return true;
 	}
 
-	private static String getValue(Card c) {
-		if (c.getValue().equals("A")) {
-			return "1";
+	private static String getValue(String value) {
+		if (value.equals("A")) {
+			return "14";
 		}
 
-		if (c.getValue().equals("K")) {
+		if (value.equals("K")) {
 			return "13";
 		}
 
-		if (c.getValue().equals("Q")) {
+		if (value.equals("Q")) {
 			return "12";
 		}
 
-		if (c.getValue().equals("J")) {
+		if (value.equals("J")) {
 			return "11";
 		}
 
-		if (c.getValue().equals("T")) {
+		if (value.equals("T")) {
 			return "10";
 		}
 
-		return c.getValue();
+		return value;
 	}
 
 	private static boolean isRoyalStraightFlush(List<Card> cards) {
@@ -229,6 +300,15 @@ class Dealer {
 			}
 		}
 		return true;
+	}
+
+	private static boolean hasXRepeatedCardsTwice(List<Card> cards) {
+		Collection<List<Card>> values = cards.stream().collect(Collectors.groupingBy(Card::getValue)).values();
+		long count = values.stream().filter(c -> c.size() == 2).count();
+		if (count == 2) {
+			return true;
+		}
+		return false;
 	}
 
 	private static boolean hasXRepeatedCards(List<Card> cards, int x) {
